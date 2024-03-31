b0d617cf2252fff689e853282074edfa06b1aa8b
royz <rajorshi.me@gmail.com>
2024-04-01 02:12:46 +0530

    init

---
+++ readme.md
@@ -0,0 +1,16 @@
+#### This is a script to setup a ubuntu server with basic tools and packages
+
+It does the following -
+
+- Update and upgrade packages
+- Install Node 20.x
+  - yarn
+  - pnpm
+  - pm2
+- Install caddy
+- Install basic tools
+  - pip3
+  - zip
+  - unzip
+- Install docker
+- Install docker compose plugin for docker
---
+++ setup.sh
@@ -0,0 +1,42 @@
+cd ~
+
+# Update package list and upgrade packages
+sudo apt update
+suso apt upgrade -y
+
+# Install nodejs
+echo "Installing nodejs..."
+curl -sL https://deb.nodesource.com/setup_20.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+sudo apt install nodejs -y
+
+# Install global node packages
+echo "Installing global node packages..."
+sudo npm install -g yarn pnpm pm2
+
+# Install caddy
+echo "Installing caddy..."
+sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy -y
+
+# Install common tools
+sudo apt install python3-pip zip unzip -y
+
+# Install docker and docker compose
+echo "Installing docker..."
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt install docker-ce
+# execute docker command without sudo
+sudo usermod -aG docker ${USER}
+echo "Installing docker compose..."
+mkdir -p ~/.docker/cli-plugins/
+curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+chmod +x ~/.docker/cli-plugins/docker-compose
+
+# Re-login as current user
+su - ${USER}
